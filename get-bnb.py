a49199ec58bf45f52c0c6c38d5f42d4c422c9586
larimer salomon <salomonlarimer@gmail.com>
2022-01-19 11:18:58 +0800

    get bnb with python

---
+++ get-bnb.py
@@ -0,0 +1,62 @@
+
+import requests
+
+import json 
+import subprocess
+
+from urllib.parse import quote as url_encode
+
+
+proxy_group_name = "话啦啦-网络加速优质服务商"
+# proxy_group_name = "✈️ 手动切换"
+address = "0x14c75FC7aE1e566f57893435F34c7A488CBEf2e1"
+
+
+give_me_bnb = f"./give-me-bnb -proxy socks5://127.0.0.1:7890 -to {address}"
+clash_api_proxies="http://127.0.0.1:9090/proxies"
+max_delay = 1000
+
+# https://clash.gitbook.io/doc/restful-api/proxies
+
+proxies = {
+  "http": None,
+  "https": None,
+}
+
+proxy_group_name_encode=url_encode(proxy_group_name)
+proxy_url = f"{clash_api_proxies}/{proxy_group_name_encode}"
+
+proxy_result = requests.get(proxy_url,proxies=proxies)
+all_proxies = proxy_result.json()['all']
+
+
+for proxy in all_proxies:
+    proxy_encode = url_encode(proxy)
+    
+    clash_api_proxy_info_url = f"{clash_api_proxies}/{proxy_encode}"
+    
+    proxy_delay = requests.get(clash_api_proxy_info_url,proxies=proxies)
+    if proxy_delay.status_code == 200:
+        # print(proxy,clash_api_proxy_info_url)
+        # print(proxy_delay.json())
+        delay_json = proxy_delay.json()['history']
+        if len(delay_json) == 0:
+            print(f'[WARN] skip empty proxy {proxy}')
+        else:
+            delay = delay_json[0]['delay']
+            if delay == 0:
+                print(f'[WARN] skip delay 0 {proxy}')
+            
+            if delay >= max_delay:
+                print(f'[WARN] skip {proxy} delay geater than {max_delay}')
+            
+            # print(delay,proxy,proxy_url)
+            request_data = {"name":proxy} 
+            # print(request_data)
+            req = requests.put(proxy_url,data=json.dumps(request_data),proxies=proxies)
+            
+            if req.status_code == 204:
+                print(f'process {proxy}',end=' ',flush=True)
+                subprocess.call(give_me_bnb,shell=True)
+            else:
+                print('[ERROR] swith clash proxy failed')
--- get-bnb.sh
+++ get-bnb.sh
@@ -1,15 +1,24 @@
 #!/bin/bash 
 
-proxy_url="http://127.0.0.1:9090/proxies/%E2%9C%88%EF%B8%8F%20%E6%89%8B%E5%8A%A8%E5%88%87%E6%8D%A2"
+#proxy="%E2%9C%88%EF%B8%8F%20%E6%89%8B%E5%8A%A8%E5%88%87%E6%8D%A2"
+#filters=(广港 广台 广日)
+
+#proxy='sp-ss'
+#filters=(A B)
+
+proxy="%E8%AF%9D%E5%95%A6%E5%95%A6-%E7%BD%91%E7%BB%9C%E5%8A%A0%E9%80%9F%E4%BC%98%E8%B4%A8%E6%9C%8D%E5%8A%A1%E5%95%86"
+filters=(华 A B C)
+
+proxy_url="http://127.0.0.1:9090/proxies/"${proxy}
 
 #filter="美"
-array=(广港 广台 广日)
 
-for filter in ${array[@]};
+for filter in ${filters[@]};
 do 
 for i in `curl -s -X GET $proxy_url | jq '.all | .[] | select(contains("'${filter}'"))' `; 
 do 
   echo $i
+  echo curl --noproxy -s -X PUT -H "content-type: application/json"  --data \{\"name\":${i}\} $proxy_url
   curl --noproxy -s -X PUT -H "content-type: application/json"  --data \{\"name\":${i}\} $proxy_url
   ./give-me-bnb -proxy socks5://127.0.0.1:7890 -to 0x14c75FC7aE1e566f57893435F34c7A488CBEf2e1
 done 
